2c34074c103cc5c3eb5fe076fc0d349812da6ab3
root <root@DESKTOP-MTRIKGC.localdomain>
2021-09-14 22:29:35 +0900

    add files

---
+++ app.py
@@ -0,0 +1,13 @@
+"""
+Flask Server for calling api by version
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def post_log():
+    return 'Create app.py file'
+
+if __name__ == '__main__':
+    app.run()
